b6391b85e15f9f8c83f8c3df2e369bd9cbebe279
Joe Rice <2Joerice@gmail.com>
2026-04-04 17:24:54 +0100

    fix: add function.go and exclude main.go

--- main.go
+++ main.go
@@ -1,46 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/joho/godotenv"
 )
-
-func init() {
-	functions.HTTP("ArtworkHandler", ArtworkHandler)
-}
-
-func ArtworkHandler(w http.ResponseWriter, r *http.Request) {
-	imageURL := r.URL.Query().Get("imageURL")
-	if imageURL == "" {
-		http.Error(w, "missing imageURL query parameter", http.StatusBadRequest)
-		return
-	}
-
-	serpAPIKey := os.Getenv("SERP_KEY")
-	vertexAIProjectID := os.Getenv("VERTEX_PROJECT_ID")
-	vertexAILocation := os.Getenv("VERTEX_PROJECT_LOCATION")
-
-	lensResult, err := serpLensSearch(imageURL, serpAPIKey)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("lens search failed: %s", err), http.StatusInternalServerError)
-		return
-	}
-
-	artworkResponse, err := generateArtworkResponse(lensResult, vertexAIProjectID, vertexAILocation)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("gemini failed: %s", err), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(artworkResponse)
-}
 
 func main() {
 	err := godotenv.Load()
